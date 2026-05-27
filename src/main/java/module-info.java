e65f93b5df9e15bd618c39ecea9088bd9ed65be8
moryota70-debug <moriyota70@gmail.com>
2026-05-27 15:55:20 +0100

    Definition de la structure et ajoute de la base de donnée

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module tg.univlome.epl.bypedu {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
     opens tg.univlome.epl.bypedu to javafx.fxml;
     exports tg.univlome.epl.bypedu;
--- src/main/java/tg/univlome/epl/bypedu/App.java
+++ src/main/java/tg/univlome/epl/bypedu/App.java
@@ -1,12 +1,14 @@
 package tg.univlome.epl.bypedu;
 
+import java.io.IOException;
+import java.sql.Connection;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import tg.univlome.epl.bypedu.DAOs.DatabaseConnection;
 
-import java.io.IOException;
 
 /**
  * JavaFX App
@@ -14,24 +16,17 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
-
+    
     @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+    public void start(Stage stage) throws IOException{
+        Connection connection = DatabaseConnection.getDatabase();
+        BorderPane root = FXMLLoader.load(getClass().getResource("accueil.fxml"));
+        scene = new Scene(root, 400, 400);
         stage.setScene(scene);
         stage.show();
     }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
-
-    public static void main(String[] args) {
+    
+    public static void main(String[] args){
         launch();
     }
 
